23195783986fd780eb29645ab7ff5c8da592ed06
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2023-08-11 23:41:58 +0100

    code added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const items = [...document.querySelectorAll(".number")];
 
 const updateCount = (el) => {
-  console.log(el);
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
 };
 
 items.forEach((item) => {
-  updateCount;
+  updateCount(item);
 });
